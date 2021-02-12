32724a4fb9845f5d75c8dceec7c8bef7802d4cc3
Raheem <raheemideadev@gmail.com>
2021-02-12 22:17:43 +0500

    dependency setup

--- app/src/main/java/xyz/teamgravity/currencyconverter/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/currencyconverter/MainActivity.kt
@@ -1,11 +1,16 @@
 package xyz.teamgravity.currencyconverter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import xyz.teamgravity.currencyconverter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
